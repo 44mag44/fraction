365ea71ca2399f2dcfe7c457aba5ec19f4fc22d1
44mag44 <antistop91@mail.ru>
2020-10-03 23:38:52 +0900

    7.5 add days and win

--- fraction/f-7/percent.c
+++ fraction/f-7/percent.c
@@ -76,27 +76,48 @@ int percentage_wins() // процент побед
 	int match; // количество матчей/игр/боев
 	float current_wr; // текущий процент побед
 	float desired_wr; // желаемый процент побед
-	int find_number; // нахождение процентов данного числа
-	int wins; // кол-во побед
+	int num_matches_days; // кол-во матчей в день
+	float day_wr; // процент побед в день
+
+	int number_wins_now; //кол-во побед сейчас
+	int without_defeat; // без поражений
+	int wins_per_days; // кол-во побед с учетом побед в день
+
+	int days_without_defeat; // кол-во дней - без пройгрышей
+	int days_per_day;	//кол-во дней - с учетом побед в день
 
 	printf("\n_");	
 	printf("\nЧисло сыгранных матчей: ");	
 	scanf("%d", &match);
 	printf("Текущий процент побед: ");	
 	scanf("%f", &current_wr);	
-	printf("Желаемый процент побед: ");	
+	printf("Желаемый общий процент побед: ");	
 	scanf("%f", &desired_wr);	
+	printf("Кол-во игр в день: ");	
+	scanf("%d", &num_matches_days);
+	printf("Процент побед в день: ");	
+	scanf("%f", &day_wr);
+	
+	// кол-во побед сейчас
+	number_wins_now=(match*current_wr)/100;
 
-	//кол-во побед сейчас
-	find_number=(match*current_wr)/100;
+	// кол-во матчей - без пройгрышей
+	without_defeat=match-((number_wins_now/desired_wr)*100);
+	printf("\nПобед без пройгрышей: %d\n", without_defeat);
 
-	//кол-во побед до желаемого результата
-	wins=(find_number/desired_wr)*100;
-	match-=wins;
+	// кол-во матчей - с учетом побед в день
+	wins_per_days=((without_defeat*day_wr)/100)+without_defeat;
+	printf("С учетом побед в день: %d\n", wins_per_days);
 
-	printf("Кол-во побед до желаемого результата: %d\n", match);
+	// кол-во дней до желаемого результата без пройгрышей
+	days_without_defeat=without_defeat/num_matches_days;
+	printf("\nДней - без пройгрышей: %d\n", days_without_defeat);
 
-	return wins;
+	// кол-во дней до желаемого результата c учетом побед/день
+	days_per_day=wins_per_days/num_matches_days;
+	printf("Дней - с учетом побед в день: %d\n", days_per_day);	
+
+	return 0;
 }
 
 int percent()
